89907d85520aff7d300a539c9ae2b804587939ba
dagxnson <dagxnson0@gmail.com>
2024-02-24 18:43:07 +0700

    abcd

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,15 @@
 #include <Servo.h>
 
-int pot0;
+int pot0=3;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  pot0 = analog.Read(A0);
-  Serial.println(pot0);
-  delay(100);
+  // pot0 = analogRead(A0);
+  // Serial.println(pot0);
+  // delay(100);
+  Serial.print('abc: ');
+  Serial.print(pot0);
 }
