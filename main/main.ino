e316cad5072e467bfcf49503fd5ec6091a69a0eb
dagxnson <dagxnson0@gmail.com>
2024-02-25 10:57:12 +0700

    'update'

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,21 @@
 #include <Servo.h>
 
-int pot0=3;
+int pot0;
+int pos0;
+Servo servo0;
 
 void setup() {
   Serial.begin(9600);
+  servo0.attach(5);
 }
 
 void loop() {
-  // pot0 = analogRead(A0);
-  // Serial.println(pot0);
-  // delay(100);
-  Serial.print('abc: ');
-  Serial.print(pot0);
+  pot0 = analogRead(A7);
+  Serial.print("pot: ");
+  Serial.println(pot0);
+  pos0 = 1024/180*pot0;
+  Serial.print("servo: ");
+  Serial.println(pos0);
+  servo0.write(0);
+  delay(100);
 }
