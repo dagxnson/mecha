3347d85533cc65fd09ba80997503ae5bac4035c1
dagxnson <dagxnson0@gmail.com>
2024-02-25 17:03:42 +0700

    'main.ino'

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,34 @@
 #include <Servo.h>
 
-int pot0;
-int pos0;
+int pot0, pos0;
+unsigned long time;
+double distance;
 Servo servo0;
 
 void setup() {
   Serial.begin(9600);
-  servo0.attach(5);
+  servo0.attach(9);
+  pinMode(4, OUTPUT);
+  pinMode(5, INPUT);
 }
 
 void loop() {
-  pot0 = analogRead(A7);
-  Serial.print("pot: ");
-  Serial.println(pot0);
-  pos0 = 1024/180*pot0;
-  Serial.print("servo: ");
+  digitalWrite(4,(0));
+  digitalWrite(4,(1));
+  delayMicroseconds(10);
+  digitalWrite(4, (0));
+  time = pulseIn (5, HIGH);
+  distance = double (time/2/29.421);
+
+  pot0 = analogRead(7);
+  pos0 = map(pot0, 0, 612, 0, 179);
+  servo0.write(pos0);
+
+  Serial.print("distance: ");
+  Serial.print(distance);
+  Serial.print("cm");
+  Serial.print("          pot: ");
+  Serial.print(pot0);
+  Serial.print("          servo: ");
   Serial.println(pos0);
-  servo0.write(0);
-  delay(100);
 }
